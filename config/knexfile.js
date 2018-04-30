ea4ea1ca39f1501872880dd45936c7a39cfb0cc4
Michael <mmccauleyjr@rogers.com>
2018-04-30 11:36:54 -0500

    Refactored knexfile.js

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,20 +1,5 @@
 // Update with your config settings.
 
-// module.exports = {
-//   development: {
-//     client: "postgresql",
-//     connection: {
-//       database: "bestinslot_dev",
-//       user: "h3lix",
-//       password: "hihi00hihi"
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     }
-//   }
-// };
-
 module.exports = function(opts = {}) {
   const required = ["client", "database", "user", "password"];
 
